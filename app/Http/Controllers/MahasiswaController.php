f18a2f954da877b28cfcee2daf1a239a7dd37040
Ozan <bakonong99@gmail.com>
2019-12-04 01:21:21 +0700

    Menambahkan fungsi migration dan model tabel students

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Student;
 
 class MahasiswaController extends Controller
 {
@@ -14,7 +15,8 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
-        $mahasiswa = DB::table('mahasiswa')->get();
+        // $mahasiswa = DB::table('students')->get();
+        $mahasiswa = Student::all();
         
         return view('mahasiswa.index', ['mahasiswa' => $mahasiswa]);
     }
